21952e84fb5ff43bcea18ca09f1fb8424ccf3604
VanTXDev <vantx2103@gmail.com>
2023-05-31 16:58:54 +0700

    Handling CRUD data on front-end and back-end

--- PythonApp/app/routes.py
+++ PythonApp/app/routes.py
@@ -4,7 +4,7 @@ from app import db
 
 @app.route("/")
 def index():
-	sql = "SELECT * FROM `todo_list`"
+	sql = "SELECT * FROM `todo_list` WHERE del_flag = '' ORDER BY date_created DESC"
 	c=db.cursor()
 	c.execute(sql)
 	result = c.fetchall()
@@ -50,13 +50,58 @@ def todo():
 	return render_template("todo.html")
 
 @app.route('/set_complete', methods=['POST'])
-def set_completed():
-	isCompleted = bool(request.json['is_completed'])
-	taskId = int(request.json['task_id'])
+def set_complete():
+	data = {}
+	try:
+		isCompleted = bool(request.json['is_completed'])
+		taskId = int(request.json['task_id'])
 
-	cur=db.cursor()
-	cur.execute("UPDATE todo_list set is_completed = (%s) WHERE id = (%s)", (isCompleted, taskId))
-	db.commit()
-	cur.close()
-	data = {"is_completed": isCompleted, "task_id": taskId}
+		cur=db.cursor()
+		cur.execute("UPDATE todo_list SET is_completed = (%s) WHERE id = (%s)", (isCompleted, taskId))
+		db.commit()
+		cur.close()
+		data = {"status": "true", "is_completed": isCompleted, "task_id": taskId, "message": "Update task status successfully!"}
+	except Exception:
+		print (Exception)
+		data  = {"status": "false", "message": "Update task status failed!"}
+	return jsonify(data)
+
+@app.route('/todo/<taskId>', methods=['GET', 'POST'])
+def todo_detail(taskId):
+	try:
+		#Get task informations
+		if request.method == 'GET':
+			cur=db.cursor()
+			cur.execute("SELECT * FROM `todo_list` WHERE id = (%s)", (taskId))
+			result = cur.fetchone()
+			cur.close()
+			return render_template('todo_detail.html', taskDetail=result)
+	
+		#Update task details
+		taskId = request.form['task_id']
+		title = request.form['title']
+		description = request.form['description']
+		is_completed = str(request.form['is_completed'])
+		is_completed = bool(is_completed)
+		cur=db.cursor()
+		cur.execute("UPDATE todo_list SET title = (%s), description = (%s), is_completed = (%s) WHERE id = (%s)", (title, description, is_completed, taskId))
+		db.commit()
+		cur.close()
+	except Exception:
+		print (Exception)
+	return redirect(url_for("index"))
+
+@app.route("/delete_task", methods=["POST"])
+def delete_task():
+	data = {}
+	try:
+		taskId = request.json['task_id']
+		cur=db.cursor()
+		cur.execute("UPDATE todo_list SET del_flag = 'DEL' WHERE id = (%s)", (taskId))
+		db.commit()
+		cur.close()
+		data = {"status": "true", "message": "Delete task successfully!"}
+	except Exception:
+		print (Exception)
+		data  = {"status": "false", "message": "Delete task failed!"}
 	return jsonify(data)
--- PythonApp/app/static/main.js
+++ PythonApp/app/static/main.js
@@ -3,46 +3,108 @@ $(document).ready(function () {
 	$(".ckb-completed").click(function (e) {
 		e.preventDefault();
 		var isCompleted = $(this).prop("checked");
-		var inputName = $(this).attr("name");
-		var taskId = inputName.slice(12, inputName.length);
+		var taskId = $(this).attr("data-task-id");
 		setCompleteStatus(taskId, isCompleted);
+	});
+	$(".btn-del-task").click(function (e) {
+		e.preventDefault();
+		var taskId = $(this).attr("data-task-id");
+		showModalConfirm(
+			"exampleModal",
+			"Do you want to delete this task?",
+			taskId
+		);
+		// delTask(taskId);
 	});
 });
 function setCompleteStatus(taskId, isCompleted) {
-	$.ajax({
-		url: "/set_complete",
-		method: "POST",
-		contentType: "application/json",
-		data: JSON.stringify({
-			task_id: taskId,
-			is_completed: isCompleted
-		}),
-		dataType: "json",
-		success: function (response) {
-			console.log(response);
+	var url = "/set_complete";
+	var data = {
+		task_id: taskId,
+		is_completed: isCompleted
+	};
+	const callBackFunc = function (response) {
+		if (response.status == "true") {
 			$(`#is_completed${taskId}`).prop("checked", response.is_completed);
 			setTaskCompletedStyle(taskId, response.is_completed);
-		},
-		error: function (e) {
-			console.log(e);
+			toastMessage(response.message, response.status);
+		} else {
+			toastMessage(response.message, response.status);
 		}
-	});
+	};
+	callAjaxPostRequest(url, data, callBackFunc);
 }
 function setTaskCompletedStyle(taskId = "", isCompleted = false) {
 	if (taskId == "") {
 		$(".ckb-completed").each(function (index) {
+			let itemNumber = $(this).attr("data-task-id");
 			if ($(this).val() == "1") {
-				$(this).parent().addClass("task-completed");
+				$(`.is_completed${itemNumber}`).addClass("task-completed");
 			} else {
-				$(this).parent().removeClass("task-completed");
+				$(`.is_completed${itemNumber}`).removeClass("task-completed");
 			}
 		});
 	} else {
-		var cardEle = $(`#is_completed${taskId}`).parent();
+		var cardEle = $(`.is_completed${taskId}`);
 		if (isCompleted) {
 			cardEle.addClass("task-completed");
 		} else {
 			cardEle.removeClass("task-completed");
 		}
 	}
+}
+
+function delTask(taskId) {
+	var url = "/delete_task";
+	var data = {
+		task_id: taskId
+	};
+	const callBackFunc = function (response) {
+		if ((response.status = "true")) {
+			$(`#task-item-${taskId}`).remove();
+		}
+		toastMessage(response.message, response.status);
+	};
+	callAjaxPostRequest(url, data, callBackFunc);
+}
+
+function toastMessage(message, status) {
+	const toastLiveEle = $("#liveToast");
+	$(".toast-body").text(message);
+	toastLiveEle.removeClass("bg-success");
+	toastLiveEle.removeClass("bg-error");
+	if (status == "true") {
+		toastLiveEle.addClass("bg-success");
+	} else {
+		toastLiveEle.addClass("bg-error");
+	}
+	const toastBootstrap = bootstrap.Toast.getOrCreateInstance(toastLiveEle);
+	toastBootstrap.show();
+}
+
+function showModalConfirm(modalId, message, taskId) {
+	$(`#${modalId}`).modal("show");
+	$(".modal-body").text(message);
+	$("#btn-accept").click(function (e) {
+		e.preventDefault();
+		delTask(taskId);
+		$(`#${modalId}`).modal("hide");
+	});
+}
+
+function callAjaxPostRequest(url, data, callBackFunc) {
+	$.ajax({
+		url: url,
+		method: "POST",
+		contentType: "application/json",
+		data: JSON.stringify(data),
+		dataType: "json",
+		success: function (response) {
+			console.log(response);
+			callBackFunc(response);
+		},
+		error: function (e) {
+			console.log(e);
+		}
+	});
 }
